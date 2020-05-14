b08ee7a009f931aa70d614507e6366b9ba6fadae
ahmedmaria <ritu329@gmail.com>
2020-05-13 22:29:28 -0400

    added checkWin and gameOver function

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var originalBoard;
 const humanPlayer="O";
 const aiPlayer="X";
-const winCombo=[
+const winCombos=[
     [0,1, 2],
     [3,4,5],
     [6,7,8],
-    [0,3,8],
+    [0,4,8],
     [2,4,6],
     [0,3,6],
     [1,4,7],
     [2,5,8]
 ];
-
+//console.log(winCombo.entries());
 const cells = document.querySelectorAll('.cell');
 startGame();
 function startGame()
@@ -38,8 +38,46 @@ turn(square.target.id, humanPlayer);
 function turn(squareId, player) {
   originalBoard[squareId] = player;
 document.getElementById(squareId).innerText=player;
-let gameWon= checkWin();
+let gameWon= checkWin(originalBoard, player);
 if (gameWon) {
   gameOver(gameWon);
 }
 }
+
+function checkWin(board, player) {
+  let plays = board.reduce((accumulator, element, index) => (element===player) ? accumulator.concat(index) : accumulator, []);
+ let gameWon = null;
+for (let [index, win] of winCombos.entries() ) {
+
+  if(win.every(element => plays.indexOf(element)>-1))
+  {
+    gameWon = {index: index, player: player};
+    break;
+  }
+}
+return gameWon;
+}
+
+function gameOver(gameWon){
+for (let index of winCombos[gameWon.index] ) {
+ document.getElementById(index).style.backgroundColor =
+  gameWon.player == humanPlayer ? "blue" : "red" ;
+  for(var i=0; i< cells.length; i++) {
+    cells[i].removeEventListener('click', turnClick, false)
+  }
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
